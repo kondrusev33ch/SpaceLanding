a074f295fc7e4c28f1e2eb658b38e1715ed31d25
kondrusev33ch <85990934+kondrusev33ch@users.noreply.github.com>
2021-10-27 15:26:30 +0300

    Add files via upload

---
+++ 2 DuelingDDQN A3C/model.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class FCDuelingQ(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 hidden_dims=(64,),
+                 activation_fc=nn.Tanh()):
+        super(FCDuelingQ, self).__init__()
+        self.activation_fc = activation_fc
+
+        self.input_layer = nn.Linear(input_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+        for i in range(len(hidden_dims) - 1):
+            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i + 1])
+            self.hidden_layers.append(hidden_layer)
+
+        self.output_value = nn.Linear(hidden_dims[-1], 1)
+        self.output_layer = nn.Linear(hidden_dims[-1], output_dim)
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def _format(self, state):
+        x = state
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, device=self.device, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, state):
+        x = self._format(state)
+        x = self.activation_fc(self.input_layer(x))
+        for hidden_layer in self.hidden_layers:
+            x = self.activation_fc(hidden_layer(x))
+
+        a = self.output_layer(x)
+        v = self.output_value(x).expand_as(a)
+        q = v + a - a.mean(1, keepdim=True).expand_as(a)
+        return q
+
+    def load(self, experiences):
+        states, actions, new_states, rewards, is_terminals = experiences
+        states = torch.from_numpy(states).float().to(self.device)
+        actions = torch.from_numpy(actions).long().to(self.device)
+        new_states = torch.from_numpy(new_states).float().to(self.device)
+        rewards = torch.from_numpy(rewards).float().to(self.device)
+        is_terminals = torch.from_numpy(is_terminals).float().to(self.device)
+        return states, actions, new_states, rewards, is_terminals
+
+
+class FCDAP(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 hidden_dims=(32, 32),
+                 activation_fc=nn.Tanh()):
+        super(FCDAP, self).__init__()
+        self.activation_fc = activation_fc
+
+        self.input_layer = nn.Linear(input_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+        for i in range(len(hidden_dims) - 1):
+            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i + 1])
+            self.hidden_layers.append(hidden_layer)
+        self.output_layer = nn.Linear(hidden_dims[-1], output_dim)
+
+    def _format(self, state):
+        x = state
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, state):
+        x = self._format(state)
+        x = self.activation_fc(self.input_layer(x))
+        for hidden_layer in self.hidden_layers:
+            x = self.activation_fc(hidden_layer(x))
+        return self.output_layer(x)
+
+    def full_pass(self, state):
+        logits = self.forward(state)
+        dist = torch.distributions.Categorical(logits=logits)
+        action = dist.sample()
+        logpa = dist.log_prob(action).unsqueeze(-1)
+        entropy = dist.entropy().unsqueeze(-1)
+        return action.item(), logpa, entropy
+
+    def select_action(self, state):
+        logits = self.forward(state)
+        dist = torch.distributions.Categorical(logits=logits)
+        action = dist.sample()
+        return action.item()
+
+    def select_greedy_action(self, state):
+        logits = self.forward(state)
+        return np.argmax(logits.detach().numpy())
+
+
+class FCV(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 hidden_dims=(32, 32),
+                 activation_fc=nn.Tanh()):
+        super(FCV, self).__init__()
+        self.activation_fc = activation_fc
+
+        self.input_layer = nn.Linear(input_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+        for i in range(len(hidden_dims) - 1):
+            hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i + 1])
+            self.hidden_layers.append(hidden_layer)
+        self.output_layer = nn.Linear(hidden_dims[-1], 1)
+
+    def _format(self, state):
+        x = state
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, dtype=torch.float32)
+            x = x.unsqueeze(0)
+        return x
+
+    def forward(self, state):
+        x = self._format(state)
+        x = self.activation_fc(self.input_layer(x))
+        for hidden_layer in self.hidden_layers:
+            x = self.activation_fc(hidden_layer(x))
+        return self.output_layer(x)
